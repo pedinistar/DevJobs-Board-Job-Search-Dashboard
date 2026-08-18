c9f35f178c933e242d0d18c00f41940ba3e0b4f2
Pedini Jayashree <134771316+pedinistar@users.noreply.github.com>
2026-08-18 10:53:26 +0530

    chore: install add configure tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button variant="secondary">Add to cart</Button>
+    </div>
+  );
 }
