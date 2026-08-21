9ed04f5ed6dccb6d478964e3e0cac835351177d4
Pedini Jayashree <134771316+pedinistar@users.noreply.github.com>
2026-08-21 19:38:45 +0530

    Clear Folders

--- src/components/Search.tsx
+++
@@ -1,9 +0,0 @@
-import { InputGroup, InputGroupInput } from "./ui/input-group";
-
-export default function Search() {
-  return (
-    <InputGroup className="w-70">
-      <InputGroupInput placeholder="Search Jobs..." />
-    </InputGroup>
-  );
-}
