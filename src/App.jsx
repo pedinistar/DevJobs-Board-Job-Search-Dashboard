1514f55da115c4bd3e0de3a345bfc9f90290c156
Pedini Jayashree <134771316+pedinistar@users.noreply.github.com>
2026-08-22 10:28:44 +0530

    feat: Location Filter

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,24 @@ import jobs from "./data/jobs";
 import JobCard from "./components/JobCard";
 import { useState } from "react";
 import SearchBar from "./components/SearchBar";
+import LocationFilter from "./components/LocationFilter";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [location, setLocation] = useState("");
 
-  const filteredJobs = jobs.filter((job) =>
-    job.title.toLowerCase().includes(searchTerm.toLowerCase()),
+  const filteredJobs = jobs.filter(
+    (job) =>
+      job.title.toLowerCase().includes(searchTerm.toLowerCase()) &&
+      job.location.toLowerCase().includes(location.toLowerCase()),
   );
 
   return (
     <div>
       <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
+      <LocationFilter location={location} setLocation={setLocation} />
+
       <div>
         {filteredJobs.map((job) => (
           <JobCard key={job.id} job={job} />
